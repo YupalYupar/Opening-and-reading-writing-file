3f8213bb15625c42014e746f975f7ca84276926b
Marsel Khachatryan <marselvkhachatryan@gmail.com>
2022-07-04 02:40:47 +0300

    Добавил 3 ДЗ

--- cook_data.py
+++ cook_data.py
@@ -2,6 +2,7 @@ from pprint import pprint
 
 
 recipe_data = "recipes.txt"
+
 cook_book = {}
 def cooking_catalog(recipe_data):
     with open(recipe_data,encoding='utf-8') as file:
@@ -23,6 +24,7 @@ def cooking_catalog(recipe_data):
     pprint(cook_book)
 
 cooking_catalog(recipe_data)
+
 print() #2 задание
 
 def get_shop_list_by_dishes(dishes, person_count):
@@ -36,3 +38,38 @@ def get_shop_list_by_dishes(dishes, person_count):
          manu[ingredient_name] = {"measure": measure, "quantity": (int(quantity) * person_count)}
   pprint(manu)
 get_shop_list_by_dishes(['Омлет','Омлет'], 2)
+
+print()#3 Задание
+from pprint import pprint
+all_txt = ['1.txt','2.txt','3.txt']
+
+def all_txt_reader(all_txt):
+    mainfile_alldict = {}
+    file_alldict = {}
+    for each_list in all_txt:
+        with open (each_list, encoding='utf-8') as one_file:
+            file_list = []
+            b = len(file_list)
+            for i in one_file:
+                file_list.append(i.strip())
+                b = str(len(file_list))
+            convertList = ' '.join([str(e) for e in file_list])
+            file_alldict = (b, convertList,)
+            mainfile_alldict[each_list] = file_alldict
+            sorted_file = sorted(mainfile_alldict.items(), key=lambda x:x[1],reverse = True)
+    return(sorted_file)
+
+pprint(all_txt_reader(all_txt))
+
+
+result_file = 'result.txt'
+def result_writing(result_file, w ):
+    with open(result_file, w , encoding='utf-8') as file:
+        for i in all_txt_reader(all_txt):
+            file.write(f'\n')
+            file.write(f'{i[0]}\n')
+            file.write(f'{i[1][0]}\n')
+            for line in range(int(i[1][0])):
+                file.write(f'Строка номер {line+1} файла номер {i[0][0]}\n')
+
+result_writing(result_file, 'w' )
